d705b92a4cd134a6898601fce39dd7fe3a16636a
Chuck Thompson <chuckthompson27@me.com>
2020-11-16 09:59:13 -0800

    Minor factor and polish.

--- test/integration/full_title_test.rb
+++ test/integration/full_title_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FullTitleTest < ActionDispatch::IntegrationTest
   test "full title helper" do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title, "Ruby Micropost App"
+    assert_equal full_title("Help"), "Help | Ruby Micropost App"
   end
 end
